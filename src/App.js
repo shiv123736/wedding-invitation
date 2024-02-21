1488c227a7270a1fbb661be94ff81c0d24a8a1de
shivamgit config --global usergit config --global user.name shivam <shiv123736@gmail.com>
2024-02-21 22:58:59 +0530

    edited whatsapp api

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
-import '../src/App.css';
+import "../src/App.css";
 import Welcome from "./components/welcome";
-import '../src/js/main.js';
-
-
-
+import "../src/js/main.js";
 
 function App() {
   return (
     <div className="App">
+      {/* <canvas className="canva"></canvas> */}
       <Welcome />
       {/* <h1>Hello Shivam, this is react app</h1> */}
     </div>
--- src/js/main.js
+++ src/js/main.js
@@ -1,56 +1,24 @@
-document.addEventListener("DOMContentLoaded", function() {
-//     // Sakura Falling Animation
-//     const sakuraSettings = {
-//         blowAnimations: ['blow-soft-left'],
-//         className: 'sakura-falling',
-//         fallSpeed: 2.5,
-//         maxSize: 12,
-//         minSize: 9,
-//         newOn: 250
-//     };
+document.addEventListener("DOMContentLoaded", function () {
+  
+  
+    // Pause Audio Function
+  // function pauseAudio(event) {
+  //     document.getElementById("my_audio").pause();
+  //     console.log('Shaadi me pakka aana');
+  //     event.stopPropagation();
+  // }
 
-// document.addEventListener("DOMContentLoaded", function () {
-//     const container = document.getElementById("sakura-container");
+  // Set the date we're counting down to
+  const countDownDate = new Date("April 18, 2024 00:00:00").getTime();
 
-//     function createSakuraPetal() {
-//         const petal = document.createElement("div");
-//         petal.classList.add("sakura-petal");
-//         petal.style.left = Math.random() * window.innerWidth + "px";
-//         container.appendChild(petal);
-
-//         setTimeout(() => {
-//             container.removeChild(petal);
-//         }, 5000); // Adjust duration as needed
-//     }
-
-//     setInterval(createSakuraPetal, 500); // Adjust interval as needed
-// // });
-
-
-
-// // Audio Play on Document Click
-// document.addEventListener('click', function() {
-//     document.getElementById("my_audio").play();
-//     console.log('Shaadi me zaroor aana');
-// });
-
-// Pause Audio Function
-// function pauseAudio(event) {
-//     document.getElementById("my_audio").pause();
-//     console.log('Shaadi me pakka aana');
-//     event.stopPropagation();
-// }
-
-
-// Set the date we're counting down to
-const countDownDate = new Date("April 18, 2024 00:00:00").getTime();
-
-// Update the count down every 1 second
-const x = setInterval(function () {
+  // Update the count down every 1 second
+  const x = setInterval(function () {
     const now = new Date().getTime();
     const distance = countDownDate - now;
     const days = Math.floor(distance / (1000 * 60 * 60 * 24));
-    const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    const hours = Math.floor(
+      (distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+    );
     const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
     const seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
@@ -61,45 +29,51 @@ const x = setInterval(function () {
             <div class='seconds block'>${seconds}<br>Seconds</div></div>`;
 
     if (distance < 0) {
-        clearInterval(x);
-        document.getElementById("time").innerHTML = "Wedding Completed !!";
+      clearInterval(x);
+      document.getElementById("time").innerHTML = "Wedding Completed !!";
     }
-}, 1000);
+  }, 1000);
 
-// Console Logs with Styles
-const styles = [
-    'background: linear-gradient(#D33106, #571402)',
-    'border: 4px solid #3E0E02',
-    'color: white',
-    'display: block',
-    'text-shadow: 0 2px 0 rgba(0, 0, 0, 0.3)',
-    'box-shadow: 0 2px 0 rgba(255, 255, 255, 0.4) inset, 0 5px 3px -5px rgba(0, 0, 0, 0.5), 0 -13px 5px -10px rgba(255, 255, 255, 0.4) inset',
-    'line-height: 40px',
-    'text-align: center',
-    'font-weight: bold',
-    'font-size: 32px'
-].join(';');
-const styles1 = [
-    'color: #FF6C37',
-    'display: block',
-    'text-shadow: 0 2px 0 rgba(0, 0, 0, 1)',
-    'line-height: 40px',
-    'font-weight: bold',
-    'font-size: 32px'
-].join(';');
-const styles2 = [
-    'color: teal',
-    'display: block',
-    'text-shadow: 0 2px 0 rgba(0, 0, 0, 1)',
-    'line-height: 40px',
-    'font-weight: bold',
-    'font-size: 32px'
-].join(';');
+  // Console Logs with Styles
+  const styles = [
+    "background: linear-gradient(#D33106, #571402)",
+    "border: 4px solid #3E0E02",
+    "color: white",
+    "display: block",
+    "text-shadow: 0 2px 0 rgba(0, 0, 0, 0.3)",
+    "box-shadow: 0 2px 0 rgba(255, 255, 255, 0.4) inset, 0 5px 3px -5px rgba(0, 0, 0, 0.5), 0 -13px 5px -10px rgba(255, 255, 255, 0.4) inset",
+    "line-height: 40px",
+    "text-align: center",
+    "font-weight: bold",
+    "font-size: 32px",
+  ].join(";");
+  const styles1 = [
+    "color: #FF6C37",
+    "display: block",
+    "text-shadow: 0 2px 0 rgba(0, 0, 0, 1)",
+    "line-height: 40px",
+    "font-weight: bold",
+    "font-size: 32px",
+  ].join(";");
+  const styles2 = [
+    "color: teal",
+    "display: block",
+    "text-shadow: 0 2px 0 rgba(0, 0, 0, 1)",
+    "line-height: 40px",
+    "font-weight: bold",
+    "font-size: 32px",
+  ].join(";");
 
-console.log('\n\n%c SAVE THE DATE: 18th April, 2024', styles);
-console.log('%cYour presence is requested!%c\n\nRegards: Suresh Kumar', styles1, styles2);
-console.log(
+  console.log("\n\n%c SAVE THE DATE: 18th April, 2024", styles);
+  console.log(
+    "%cYour presence is requested!%c\n\nRegards: Suresh Kumar",
+    styles1,
+    styles2
+  );
+  console.log(
     `%c शादी में जलूल जलूल आना होगा \n\n`,
-    'color: yellow; background:tomato; font-size: 24pt; font-weight: bold'
-);
+    "color: yellow; background:tomato; font-size: 24pt; font-weight: bold"
+  );
 });
+
+
